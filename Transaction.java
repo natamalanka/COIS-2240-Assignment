539875493147bdbb7f3d1c05e6a806b7a9509350
Natan M. Student <natanmalanka@trentu.ca>
2024-12-04 20:45:57 -0500

    Task 2
    Modified the Transcaction.java to allow saving data in transactions.txt.

--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -24,6 +27,7 @@ public class Transaction {
             member.borrowBook(book);
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -38,8 +42,20 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
         } else {
             System.out.println("This book was not borrowed by the member.");
+        }
+    }
+
+    // Save transaction details to a file
+    private void saveTransaction(String transactionDetails) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.txt", true))) {
+            writer.write(transactionDetails);
+            writer.newLine();
+        } catch (IOException e) {
+        	// Error out if the transaction couldn't save
+            System.out.println("Error saving transaction: " + e.getMessage());
         }
     }
 
